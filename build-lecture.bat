f7b2ad3222768cb798bfc6b687416e9edcd28e05
James Keirstead <james.keirstead@gmail.com>
2011-09-09 20:58:21 +0100

    First working version.  Includes example source file, Python and batch build scripts, but no styles.

---
+++ build-lecture.bat
@@ -0,0 +1,22 @@
+:: Batch file for building lecture notes
+:: James Keirstead
+:: 9 September 2011
+
+@ECHO OFF
+
+:: Change this directory as appropriate to your installation
+SET SCRIPTDIR=d:\jkeirste\teaching\lectures\python
+
+:: Only run the analysis if there are two arguments
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+
+if %argC% == 2 (
+   echo Building lecture '%1' with style '%2'
+   python %SCRIPTDIR%\build-lecture.py %1 %2 > build.log
+   echo Build complete.  Further information in 'build.log'.
+) ELSE (
+  echo Need two arguments, the content file 1 and the style file 2
+)
+
+
---
+++ python/build-lecture.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+"""build-lecture.py: Builds lecture notes and slides from a common LaTeX source document."""
+
+import sys
+import os
+import string
+import subprocess
+import glob
+
+__author__ = "James Keirstead"
+__copyright__ = "Copyright 2011, James Keirstead"
+
+
+# Define a function to build the file
+def build_file(content, style, isSlide):
+    """Builds and compiles the lecture files with a given style 
+
+    Keyword arguments:
+    content -- the content file
+    style   -- the style file
+    isSlide -- True, if slides should be built.  False, if notes.
+    """
+
+    # Set up some initial variables
+    root = 'slides' if isSlide else 'notes'
+    work_dir = os.path.dirname(__file__)
+
+    # Choose the right header file
+    header_file = work_dir + os.altsep + root + '-header.tex'
+    
+    # Read the template
+    f = open(header_file, 'r')
+    lines = f.readlines()
+    f.close()
+
+    # Create the source file
+    src_file = root + '.tex'
+    source = open(src_file, 'w')
+    
+    for line in lines:
+        source.write(line)
+
+    if (isSlide):
+        # This code needs to be inserted before the content
+        source.write('%% Necessary to ensure that the bibliography shows up correctly\n')
+        source.write('\mode<presentation>{\def\\newblock{}}\n')
+
+    source.write('\input{' + style + '}\n')
+    source.write('\input{' + content + '}\n')
+    source.close()
+
+    # Compile the result
+    commands = ['xelatex','bibtex','xelatex','xelatex']
+    for command in commands:
+        subprocess.call((command, root)) 
+
+    # Tidy up
+    bad_exts = ['aux','bbl','nav','out','blg','snm','toc']
+    bad_exts = [(root + '.' + ext) for ext in bad_exts]
+
+    for root, dirs, files in os.walk(os.getcwd()):
+        for trace in bad_exts:
+            win_trace_path = os.path.join(root, trace)
+            for filename in glob.glob(win_trace_path):
+                if os.path.exists(filename):
+                    os.remove(filename)
+ 
+# Read the command line arguments
+content = sys.argv[1]
+style = sys.argv[2]
+
+# Build the files
+build_file(content, style, True)
+build_file(content, style, False)
